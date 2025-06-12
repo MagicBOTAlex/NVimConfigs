d704a7bdc961cd1b405a1ba8adf20e765ea34410
BOTAlex <zhentao2004@gmail.com>
2025-06-12 16:44:16 +0200

    Unified clipboard experience (Only OCS52)

---
+++ lua/platforms/windows.lua
@@ -0,0 +1,28 @@
+local is_windows = vim.loop.os_uname().sysname:match("Windows")
+if not is_windows then
+  return
+end
+
+local file = "shada/main.shada.tmp.X"
+local path = vim.fn.stdpath("data") .. "/" .. file
+
+local stat = vim.loop.fs_stat(path)
+if stat then
+  local ok, err = os.remove(path)
+  if not ok then
+    print("Failed to delete file: " .. err)
+  end
+end
+
+-- OSC-52 clipboard
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
