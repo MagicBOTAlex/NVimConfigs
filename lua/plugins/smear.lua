7a3cba86eefa78df84177140f9bd04ef7e32fdfe
MagicBOTAlex <zhentao2004@gmail.com>
2026-05-11 09:50:09 +0200

    i want smear back

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -1,13 +1,21 @@
 return {
---   "sphamba/smear-cursor.nvim",
---
---   opts = {                                -- Default  Range
---     stiffness = 0.8,                      -- 0.6      [0, 1]
---     trailing_stiffness = 0.5,             -- 0.4      [0, 1]
---     stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
---     trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
---     damping = 0.8,                        -- 0.65     [0, 1]
---     distance_stop_animating = 0.5,        -- 0.1      > 0
---   time_interval = 30
--- },
+  "sphamba/smear-cursor.nvim",
+  -- The enabled function determines if the plugin loads
+  enabled = function()
+    local max_filesize = 1024 * 1024 -- 1MB threshold
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(0))
+    if ok and stats and stats.size > max_filesize then
+      return false
+    end
+    return true
+  end,
+  opts = {
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    stiffness_insert_mode = 0.7,
+    trailing_stiffness_insert_mode = 0.7,
+    damping = 0.8,
+    distance_stop_animating = 0.5,
+    time_interval = 30,
+  },
 }
